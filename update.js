f749592dccd368f52174614e56be83c4dc64dd04
Justin Schneider <justincase18@msn.com>
2018-08-06 10:10:18 -0800

    added isVolumeUp function

--- update.js
+++ update.js
@@ -15,30 +15,26 @@ axios.get("https://min-api.cryptocompare.com/data/all/coinlist")
     Object.entries(response.data.Data).forEach((coin, i) => {
         setTimeout(() => {
             getCoinHist(coin[1].Symbol)            
-        }, i*1000);
+        }, i*1000)
      })     
 })
 
-const getCoinHist = (coinSymbol) => {
-    
-    const url = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + coinSymbol + "&tsym=BTC&allData=true";
+const getCoinHist = (coinSymbol) => {    
+    const url = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + coinSymbol + "&tsym=BTC&allData=true"
     axios.get(url)
     .then(response => {
-        let data = response.data.Data
-        
+        let data = response.data.Data        
         if  ( isRlz(data) && isPriceDown(data) && isVolumeUp(data) ) {
             console.log("adding " + coinSymbol + " to table")             
         } else {
             console.log("removing " + coinSymbol + " from table")
-        }
-        
+        }        
     })
     .catch(err => console.log(err))
 }
 
 const isRlz = (priceHistory) => {
-
-    let highPrice = 0;
+    let highPrice = 0
     let currentPrice = priceHistory[0].close
 
     priceHistory.forEach(day => {
@@ -46,7 +42,6 @@ const isRlz = (priceHistory) => {
     })
 
     return currentPrice < highPrice * .786 ? true : false  
-
 }
 
 const isPriceDown = (priceHistory) => {
@@ -54,18 +49,29 @@ const isPriceDown = (priceHistory) => {
     let sumThirty = 0
 
     // create nine and thirty day totals
-    for (let i = 0; i < 29; i++) {
-        if (i < 9) {
+    for (let i = 0; i < 18; i++) {
+        if (i < 6) {
             sumNine += priceHistory[i].close  
         }
         sumThirty += priceHistory[i].close
     }
 
     //return true if thirty day average is greater than nine day average
-    return (sumThirty / 30) > (sumNine / 9) ? true : false
+    return (sumThirty / 18) > (sumNine / 6) ? true : false
 }
 
 const isVolumeUp = (priceHistory) => {
-    return true
-    // return true if volume 9MA > 30MA
+    let sumNine = 0
+    let sumThirty = 0
+
+    // create nine and thirty day totals
+    for (let i = 0; i < 18; i++) {
+        if (i < 6) {
+            sumNine += priceHistory[i].volumeto  
+        }
+        sumThirty += priceHistory[i].volumeto
+    }
+
+    //return true if thirty day average is less than nine day average
+    return (sumThirty / 18) < (sumNine / 6) ? true : false
 }
