bd19c913f76a85a9590f9474adde0f2b5d984241
Justin Schneider <justincase18@msn.com>
2018-08-08 12:30:06 -0800

    Added better comments

--- update.js
+++ update.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const knex = require('knex')
 const pg = require('pg')
 
+// Create connection for Postgres Database.
 const db = knex({
     client: 'pg',
     connection: {
@@ -10,8 +11,10 @@ const db = knex({
     }
 })
 
+// Get list of coins from cryptocompare.
+// For each coin, call getCoinHist with delay to avoid exceeding rate limit.
 axios.get("https://min-api.cryptocompare.com/data/all/coinlist")
-.then(response => { 
+.then(response => {    
     Object.entries(response.data.Data).forEach((coin, i) => {
         setTimeout(() => {
             getCoinHist(coin[1].Symbol)            
@@ -19,10 +22,14 @@ axios.get("https://min-api.cryptocompare.com/data/all/coinlist")
      })     
 })
 
-const getCoinHist = (coinSymbol) => {    
+const getCoinHist = (coinSymbol) => {
+
+    // Get all price data for the coin.
     const url = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + coinSymbol + "&tsym=BTC&allData=true"
     axios.get(url)
     .then(response => {
+
+        // Add coin to table if price is in RLZ, price is trending down, and volume is trending up.
         let data = response.data.Data        
         if  ( isRlz(data) && isPriceDown(data) && isVolumeUp(data) ) {
             console.log("adding " + coinSymbol + " to table")             
@@ -38,42 +45,45 @@ const isRlz = (priceHistory) => {
     let currentPrice = priceHistory[0].close
     let lowPrice = currentPrice
 
+    // Iterate through each day, adjusting highPrice or lowPrice if currentPrice is outside that range.
     priceHistory.forEach(day => {
         highPrice = day.close > highPrice ? day.close : highPrice
         lowPrice = day.close < lowPrice ? day.close : lowPrice
     })
+
+    // Calculate the rlz (78.6% of range) and return true if current price is less than rlz.
     let rlz = highPrice - ((highPrice - lowPrice) * .786)
     return currentPrice < rlz ? true : false  
 }
 
 const isPriceDown = (priceHistory) => {
     let sumNine = 0
-    let sumThirty = 0
+    let sumEighteen = 0
 
-    // create nine and thirty day totals
+    // Create nine and eighteen day totals.
     for (let i = 0; i < 18; i++) {
         if (i < 6) {
             sumNine += priceHistory[i].close  
         }
-        sumThirty += priceHistory[i].close
+        sumEighteen += priceHistory[i].close
     }
 
-    //return true if thirty day average is greater than nine day average
-    return (sumThirty / 18) > (sumNine / 6) ? true : false
+    // Return true if eighteen day average is greater than nine day average.
+    return (sumEighteen / 18) > (sumNine / 6) ? true : false
 }
 
 const isVolumeUp = (priceHistory) => {
     let sumNine = 0
-    let sumThirty = 0
+    let sumEighteen = 0
 
-    // create nine and thirty day totals
+    // Create nine and eighteen day totals.
     for (let i = 0; i < 18; i++) {
         if (i < 6) {
             sumNine += priceHistory[i].volumeto  
         }
-        sumThirty += priceHistory[i].volumeto
+        sumEighteen += priceHistory[i].volumeto
     }
 
-    //return true if thirty day average is less than nine day average
-    return (sumThirty / 18) < (sumNine / 6) ? true : false
+    // Return true if eighteen day average is less than nine day average.
+    return (sumEighteen / 18) < (sumNine / 6) ? true : false
 }
